2b2ed17859d3c0297a2e501da92c4b3596132e01
Lander Van laer <lander.vanlaer1@gmail.com>
2022-11-01 11:43:54 +0100

    Add goto /session after successful session creation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 	import { User } from '../lib/types/User';
 	import { SessionStore, UserStore } from '../stores';
 	import Spinner from '$lib/components/Spinner.svelte';
-	import { AxiosError } from 'axios';
 	import { apiGetErrorMessage } from '../axios';
+	import { goto } from '$app/navigation';
 	
 	enum Status {
 		CHOOSER, CREATE, JOIN
@@ -29,11 +29,10 @@
 			const res: [Session, User] = await createSessionPromise;
 			SessionStore.set(res[0]);
 			UserStore.set(res[1]);
-		} catch (e: AxiosError | Error) {
+			await goto('/session');
+		} catch (e) {
 			formErrorMessage = apiGetErrorMessage(e);
 		}
-		
-		//TODO Redirect to session page
 	};
 </script>
 
@@ -42,7 +41,7 @@
 	<hr>
 	<div class='w-75 m-auto'>
 		{#if status === Status.CHOOSER}
-			<div id='choose' class='d-flex gap-5 mt-5'>
+			<div id='choose' class='d-flex gap-2 mt-5'>
 				<button on:click={changeStatus(Status.CREATE)}
 								class='card w-100 text-center pt-4 pb-4 bg-primary text-white'>
 					<span class='m-auto'>Create</span>
@@ -60,9 +59,9 @@
 			{#if status === Status.CREATE}
 				<form on:submit|preventDefault={submitCreate} class='align-baseline mt-5 text-center'>
 					{#if formErrorMessage}<p class='alert alert-danger'>{formErrorMessage}</p>{/if}
-					<input class='align-middle' maxlength='15' placeholder='Username' type='text' name='name' id='name'>
-					<button type='submit' class='btn btn-primary'>Create</button>
-					<Spinner class='align-middle text-primary' promise={createSessionPromise} />
+					<input class='align-middle mt-2' maxlength='15' placeholder='Username' type='text' name='name' id='name'>
+					<button type='submit' class='btn btn-primary mt-2'>Create</button>
+					<Spinner class='align-middle text-primary mt-2' promise={createSessionPromise} />
 				</form>
 			{/if}
 		{/if}
